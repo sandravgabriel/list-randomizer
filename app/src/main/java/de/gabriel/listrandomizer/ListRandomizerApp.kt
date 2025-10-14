f25e2791fd2995de254e13223d8a561e59d79d4e
Sandra Gabriel <sandra.gabriel@mni.thm.de>
2025-10-14 15:43:48 +0200

    Add safe area padding to FAB in ListRandomizerApp
    Applied WindowInsets.safeDrawing padding to the FloatingActionButton to ensure it is not obscured by system UI elements.

--- app/src/main/java/de/gabriel/listrandomizer/ListRandomizerApp.kt
+++ app/src/main/java/de/gabriel/listrandomizer/ListRandomizerApp.kt
@@ -7,9 +7,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -43,6 +47,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -138,6 +143,14 @@ fun ListRandomizerApp(
                                 modifier = Modifier
                                     .align(Alignment.BottomEnd)
                                     .padding(16.dp)
+                                    .padding(
+                                        end = WindowInsets.safeDrawing
+                                            .asPaddingValues()
+                                            .calculateEndPadding(LocalLayoutDirection.current),
+                                        bottom = WindowInsets.safeDrawing
+                                            .asPaddingValues()
+                                            .calculateBottomPadding()
+                                    )
                             ) {
                                 Icon(Icons.Default.Add, contentDescription = stringResource(R.string.item_entry_title))
                             }
